98fa0c84c58109ba45577d9feb3ce2777554fdaa
Arthur Dufour <arthur@road-b-score.com>
2021-05-18 19:50:00 +0200

    wip(register): validate-email email

--- app/Listeners/User.ts
+++ app/Listeners/User.ts
@@ -1,5 +1,5 @@
 import { EventsList } from '@ioc:Adonis/Core/Event'
-// import Env from '@ioc:Adonis/Core/Env'
+import Mail from '@ioc:Adonis/Addons/Mail'
 import Logger from '@ioc:Adonis/Core/Logger'
 
 export default class User {
@@ -7,7 +7,17 @@ export default class User {
     user.activationCode = Math.floor(1000 + Math.random() * 9000).toString()
     await user.save()
 
-    // TODO: send email
     Logger.info(`${user.email}'s code is ${user.activationCode}`)
+
+    await Mail.send((message) => {
+      message
+        .from('noreply@delinq.io')
+        .to(user.email)
+        .subject('delinq.io email verification code')
+        .htmlView('emails/validate-email', {
+          user: { username: user.username },
+          code: user.activationCode,
+        })
+    })
   }
 }
