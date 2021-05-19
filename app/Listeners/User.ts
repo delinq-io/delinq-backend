e8b8d8648fc19970eca1f41d2fc964ba78b4007d
Arthur Dufour <arthur@road-b-score.com>
2021-05-19 11:00:16 +0200

    refactor(validate-email): remove activationCode logging

--- app/Listeners/User.ts
+++ app/Listeners/User.ts
@@ -1,13 +1,10 @@
 import { EventsList } from '@ioc:Adonis/Core/Event'
 import Mail from '@ioc:Adonis/Addons/Mail'
-import Logger from '@ioc:Adonis/Core/Logger'
 
 export default class User {
   public async onNewUser (user: EventsList['new:user']) {
     user.activationCode = Math.floor(1000 + Math.random() * 9000).toString()
     await user.save()
-
-    Logger.info(`${user.email}'s code is ${user.activationCode}`)
 
     await Mail.send((message) => {
       message
